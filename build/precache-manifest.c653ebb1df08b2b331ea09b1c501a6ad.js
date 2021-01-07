5dc5a242cd689831a173da8e9a58bf7760d7b6f8
karenchang-dev <karenchang.dev@gmail.com>
2021-01-07 14:27:27 +1100

    github page deployment

---
+++ build/precache-manifest.c653ebb1df08b2b331ea09b1c501a6ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce38b4b0c34cbf39d93d0e3a0404d59",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "c87654e788049f87b003",
+    "url": "/keeper/static/js/2.8dfbbd3e.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/keeper/static/js/2.8dfbbd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a128f735674e12242821",
+    "url": "/keeper/static/js/main.18f1b887.chunk.js"
+  },
+  {
+    "revision": "71f7bc87a051dc8cd391",
+    "url": "/keeper/static/js/runtime-main.4ac50011.js"
+  }
+]);
